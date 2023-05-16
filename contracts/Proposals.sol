4b32d22d6fe1630bc6c9356b62fb2f769759f6c2
雍周文 <2547953859@qq.com>
2023-05-16 16:57:52 +0800

    Proposal update

--- contracts/Proposals.sol
+++ contracts/Proposals.sol
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
+
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "./ProposalLinkedList.sol";
 import "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
+
+import "./ProposalLinkedList.sol";
 
 contract Proposals {
     bytes32 public ramdomNumber =
