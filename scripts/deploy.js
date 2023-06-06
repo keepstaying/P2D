2d0bb893b0238aa9e4e9409ad376bf48c27097c5
keepstaying <78075596+keepstaying@users.noreply.github.com>
2023-06-06 13:16:09 +0800

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,39 +3,39 @@ const { ethers } = require(`hardhat`);
 
 async function deploy() {
 
-    const [deployer] = await ethers.getSigners();
+    const [deployer] = await ethers.getSigners();//定义一个实例
 
     const lixiangAddress = '0xeea78E4B6bf6BE675f96Bb4cbeDFd7a89Bdb18f6';
 
     console.log(
         "Deploying contracts with the account:",
-        deployer.address
+        deployer.address//当前账户的地址
     );
 
-    console.log("Account balance:", (await deployer.getBalance()).toString());
+    console.log("Account balance:", (await deployer.getBalance()).toString());//当前账户的余额
 
-    const Token = await ethers.getContractFactory("VicToken");
-    const VicToken = await Token.deploy("vicToken", "Vic");
+    const Token = await ethers.getContractFactory("VicToken"); //创建合约
+    const VicToken = await Token.deploy("vicToken", "Vic");//部署token合约
 
-    console.log("Token address:", VicToken.address);
+    console.log("Token address:", VicToken.address);//打印token合约的地址
 
-    const NFT = await ethers.getContractFactory("MyERC721");
-    const VicNFT = await NFT.deploy("vicNFT", "VVV")
+    const NFT = await ethers.getContractFactory("MyERC721");//创建NFT合约
+    const VicNFT = await NFT.deploy("vicNFT", "VVV")//部署NFT合约
 
-    console.log("NFT address:", VicNFT.address);
+    console.log("NFT address:", VicNFT.address);//打印地址
 
 
-    const Proposal = await ethers.getContractFactory("VicProposal");
-    const VicProposal = await Proposal.deploy(VicToken.address);
+    const Proposal = await ethers.getContractFactory("VicProposal");//创建提案合约
+    const VicProposal = await Proposal.deploy(VicToken.address);//部署合约并将token合约的地址作为参数传入
 
     console.log("Proposal address:", VicProposal.address);
 
-    const Dao = await ethers.getContractFactory("VicDAO");
-    const VicDao = await Dao.deploy(VicToken.address, VicProposal.address);
+    const Dao = await ethers.getContractFactory("VicDAO");                   //创建Dao的合约
+    const VicDao = await Dao.deploy(VicToken.address, VicProposal.address);  //部署Dao合约并将token合约和提案合约的参数传入
 
     console.log("VicDao address:", VicDao.address);
 
-    console.log("给部署者地址铸造代币")
+    console.log("给部署者地址铸造代币")                                      
     await VicToken.mint(deployer.address, 100000);
     const tokenBalance = await VicToken.balanceOf(deployer.address);
     console.log("成功铸造:" + ethers.utils.formatUnits(tokenBalance, 0));
